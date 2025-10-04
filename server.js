a2f4ac6938e5aaaf93f2c5f00b42b8f35efcfa28
jungwooh2 <samsnnahg@gmail.com>
2025-10-04 15:27:06 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const express = require("express");
 const cors = require("cors");
 
 
